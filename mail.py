59033ef9d2fd2b05a52dc38b1968e7722ca0fcf6
Alex Mueller <alexarnimueller@gmail.com>
2019-07-31 16:34:52 +0100

    working with image as attachment

--- mail.py
+++ mail.py
@@ -1,29 +1,20 @@
 import smtplib
 import imghdr
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-
-# Email you want to send the update from (only works with gmail)
-fromEmail = 'alexarnimueller@gmail.com'
-# You can generate an app password here to avoid storing your password in plain text
-# https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = 'xublapmotrygawmj'
-
-# Email you want to send the update to
-toEmail = 'arnimueller.alex@bluewin.ch'
+from email.message import EmailMessage
 
 def sendEmail(image):
-	msgRoot = MIMEMultipart('related')
-	msgRoot['Subject'] = 'Security Camera Update'
-	msgRoot['From'] = fromEmail
-	msgRoot['To'] = toEmail
-	msgRoot.preamble = 'Raspberry pi security camera update'
-	msgText = MIMEText('Smart security cam found object')
-	msgAlternative.attach(msgText)
-	msgRoot.add_attachment(image, maintype='image', subtype=imghdr.what(None, image))
-	smtp = smtplib.SMTP('smtp.gmail.com', 587)
-	smtp.starttls()
-	smtp.login(fromEmail, fromEmailPassword)
-	smtp.sendmail(fromEmail, toEmail, msgRoot.as_string())
-	smtp.quit()
+    fromEmail = 'alexarnimueller@gmail.com'
+    fromEmailPassword = 'xublapmotrygawmj'
+    toEmail = 'arnimueller.alex@bluewin.ch'
+        
+    msg = EmailMessage()
+    msg['Subject'] = 'Security Camera Update'
+    msg['From'] = fromEmail
+    msg['To'] = toEmail
+    msg.preamble = 'Raspberry pi security camera update'
+    msg.add_attachment(image, maintype='image', subtype=imghdr.what(None, image))
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.starttls()
+    smtp.login(fromEmail, fromEmailPassword)
+    smtp.send_message(msg)
+    smtp.quit()
